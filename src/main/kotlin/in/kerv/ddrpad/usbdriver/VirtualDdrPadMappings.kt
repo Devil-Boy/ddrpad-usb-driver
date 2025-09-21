3fea055a833b1d2c9729ed153e00fe04c166c04b
Devil Boy <github@kerv.in>
2025-09-20 20:46:39 -0700

    Adjusted mappings to avoid the dpad on the arrows.
    The dpad creates the "axis issue" where left+right or up+down can't be pressed simulatneously

--- src/main/kotlin/in/kerv/ddrpad/usbdriver/VirtualDdrPadMappings.kt
+++ src/main/kotlin/in/kerv/ddrpad/usbdriver/VirtualDdrPadMappings.kt
@@ -7,24 +7,24 @@ object VirtualDdrPadMappings {
 
   val realToVirtual =
       mapOf(
-          DdrPadButton.UP to EventCode.BTN_DPAD_UP,
-          DdrPadButton.DOWN to EventCode.BTN_DPAD_DOWN,
-          DdrPadButton.LEFT to EventCode.BTN_DPAD_LEFT,
-          DdrPadButton.RIGHT to EventCode.BTN_DPAD_RIGHT,
-          DdrPadButton.START to EventCode.BTN_START,
-          DdrPadButton.SELECT to EventCode.BTN_SELECT,
+          DdrPadButton.UP to EventCode.BTN_NORTH,
+          DdrPadButton.DOWN to EventCode.BTN_SOUTH,
+          DdrPadButton.LEFT to EventCode.BTN_WEST,
+          DdrPadButton.RIGHT to EventCode.BTN_EAST,
+          DdrPadButton.START to EventCode.BTN_START, // Project Outfox shows this as "Dpad Right"
+          DdrPadButton.SELECT to EventCode.BTN_SELECT, // Project Outfox shows this as "Dpad Left"
 
           // Cross
-          DdrPadButton.UP_LEFT to EventCode.BTN_SOUTH,
+          DdrPadButton.UP_LEFT to EventCode.BTN_TL, // Project Outfox shows this as "Back"
 
           // Circle
-          DdrPadButton.UP_RIGHT to EventCode.BTN_EAST,
+          DdrPadButton.UP_RIGHT to EventCode.BTN_TR, // Project Outfox shows this as "Start"
 
           // Triangle
-          DdrPadButton.DOWN_LEFT to EventCode.BTN_NORTH,
+          DdrPadButton.DOWN_LEFT to EventCode.BTN_TL2,
 
           // Square
-          DdrPadButton.DOWN_RIGHT to EventCode.BTN_WEST,
+          DdrPadButton.DOWN_RIGHT to EventCode.BTN_TR2,
       )
 
   fun DdrPadButton.toEventCode() = realToVirtual[this]
