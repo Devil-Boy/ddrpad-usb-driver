b0e930cc1842e8cdaa597e6e9283b0b74eba833b
Devil Boy <github@kerv.in>
2025-10-17 01:32:25 -0700

    Attempted to add handling for the different styles of control bytes we've been seeing.
    Unfortunately, I'm pretty sure there's now an unacceptable amount of input delay. It's likely to do with additional 2-layers of map lookups on every "tick"

--- src/main/kotlin/in/kerv/ddrpad/usbdriver/DdrPadInputProcessor.kt
+++ src/main/kotlin/in/kerv/ddrpad/usbdriver/DdrPadInputProcessor.kt
@@ -3,35 +3,36 @@ package `in`.kerv.ddrpad.usbdriver
 @OptIn(ExperimentalUnsignedTypes::class)
 object DdrPadInputProcessor {
 
-  /**
-   * Converts a raw byte array received from the DDRPad USB device into a [ControlBytes] object.
-   */
+  /** Converts a raw byte array received from the DDRPad USB device into a [ControlBytes] object. */
   fun ByteArray.toControlBytes() = ControlBytes(this.toUByteArray())
 
   /**
-   * A value class representing the two control bytes received from the DDRPad.
-   * These bytes encode the state of the buttons.
+   * A value class representing the two control bytes received from the DDRPad. These bytes encode
+   * the state of the buttons.
    *
-   * Using a value class helps us avoid initializing a new objects in the heap while also allowing us to code object-orientedly.
+   * Using a value class helps us avoid initializing a new objects in the heap while also allowing
+   * us to code object-orientedly.
    */
   @JvmInline
   value class ControlBytes(val bytes: UByteArray) {
-    init {
-      //require(bytes.size == 8) { "The DDRPad always has 8 control bytes!" }
-    }
 
     /**
      * Returns a set of [DdrPadButton]s that are currently pressed, based on the control bytes.
      *
      * @return A [Set] of [DdrPadButton]s that are pressed.
      */
-    fun getPressedButtons() = DdrPadButton.entries.filter {
-      bytes[it.controlByteIndex] and it.bitMask != 0.toUByte()
-    }.toSet()
+    fun getPressedButtons(): Set<DdrPadButton> {
+      val controlScheme = inputBytesCountToControlSchemes[bytes.size]
+      if (controlScheme == null) {
+        throw AssertionError()
+      }
+
+      return controlScheme.filterValues { bytes[it.byteIndex] and it.bitMask != 0.toUByte() }.keys
+    }
 
     /**
-     * Checks if the button states represented by this [ControlBytes] object
-     * are different from another [ControlBytes] object.
+     * Checks if the button states represented by this [ControlBytes] object are different from
+     * another [ControlBytes] object.
      *
      * This is how we efficiently detect changes.
      *
@@ -41,27 +42,76 @@ object DdrPadInputProcessor {
     fun changedFrom(other: ControlBytes) = !bytes.contentEquals(other.bytes)
 
     /**
-     * Returns a string representation of the currently pressed buttons for debugging/development purposes.
+     * Returns a string representation of the currently pressed buttons for debugging/development
+     * purposes.
      */
-    override fun toString() = "Raw: [${bytes.toBinaryStrings()}], Parsed: ${getPressedButtons()}"
+    override fun toString() =
+        "Raw: ${bytes.size}[${bytes.toBinaryStrings()}], Parsed: ${getPressedButtons()}"
 
-    private fun UByteArray.toBinaryStrings() = this.joinToString { it.toString(2).padStart(8, '0')  }
+    private fun UByteArray.toBinaryStrings() = this.joinToString { it.toString(2).padStart(8, '0') }
   }
 
-  /** Represents the individual buttons on a DDRPad, along with their control byte index and bitmask. */
-  enum class DdrPadButton(val controlByteIndex: Int, val bitMask: UByte) {
-    UP(2, 0b0010000.toUByte()),
-    DOWN(2, 0b01000000.toUByte()),
-    LEFT(2, 0b10000000.toUByte()),
-    RIGHT(2, 0b00100000.toUByte()),
-
-    UP_LEFT(3, 0b00100000.toUByte()),
-    UP_RIGHT(3, 0b01000000.toUByte()),
-    DOWN_LEFT(3, 0b10000000.toUByte()),
-    DOWN_RIGHT(3, 0b00010000.toUByte()),
-
-    START(2, 0b00001000.toUByte()),
-    SELECT(2, 0b00000001.toUByte()),
+  /**
+   * Represents the individual buttons on a DDRPad, along with their control byte index and bitmask.
+   */
+  enum class DdrPadButton {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    UP_LEFT,
+    UP_RIGHT,
+    DOWN_LEFT,
+    DOWN_RIGHT,
+    START,
+    SELECT,
   }
 
+  data class ControlSignature(val byteIndex: Int, val bitMask: UByte)
+
+  val controlSchemeA =
+      mapOf(
+          DdrPadButton.UP to ControlSignature(2, 0b0000001.toUByte()),
+          DdrPadButton.DOWN to ControlSignature(2, 0b0000010.toUByte()),
+          DdrPadButton.LEFT to ControlSignature(2, 0b0000100.toUByte()),
+          DdrPadButton.RIGHT to ControlSignature(2, 0b0001000.toUByte()),
+          DdrPadButton.UP_LEFT to ControlSignature(4, 0b11111111.toUByte()),
+          DdrPadButton.UP_RIGHT to ControlSignature(5, 0b11111111.toUByte()),
+          DdrPadButton.DOWN_LEFT to ControlSignature(7, 0b11111111.toUByte()),
+          DdrPadButton.DOWN_RIGHT to ControlSignature(6, 0b11111111.toUByte()),
+          DdrPadButton.START to ControlSignature(2, 0b00010000.toUByte()),
+          DdrPadButton.SELECT to ControlSignature(2, 0b00100000.toUByte()),
+      )
+
+  val controlSchemeB =
+    mapOf(
+      DdrPadButton.UP to ControlSignature(2, 0b0010000.toUByte()),
+      DdrPadButton.DOWN to ControlSignature(2, 0b01000000.toUByte()),
+      DdrPadButton.LEFT to ControlSignature(2, 0b10000000.toUByte()),
+      DdrPadButton.RIGHT to ControlSignature(2, 0b00100000.toUByte()),
+      DdrPadButton.UP_LEFT to ControlSignature(3, 0b00100000.toUByte()),
+      DdrPadButton.UP_RIGHT to ControlSignature(3, 0b01000000.toUByte()),
+      DdrPadButton.DOWN_LEFT to ControlSignature(3, 0b10000000.toUByte()),
+      DdrPadButton.DOWN_RIGHT to ControlSignature(3, 0b00010000.toUByte()),
+      DdrPadButton.START to ControlSignature(2, 0b00001000.toUByte()),
+      DdrPadButton.SELECT to ControlSignature(2, 0b00000001.toUByte()),
+    )
+
+  // Right now, we're actually not sure why the input bytes seems to randomly change format.
+  // Hopefully, we can identify which format is being used based on how many bytes we are receiving.
+  val inputBytesCountToControlSchemes =
+      mapOf(
+          8 to controlSchemeA,
+          // TODO: Figure out how many bytes control scheme A has
+          -1 to controlSchemeB,
+      )
+
+  /**
+   * Checks if the given number of bytes corresponds to a recognized control scheme.
+   *
+   * @param numberOfBytes The number of bytes received from the DDRPad.
+   * @return `true` if the number of bytes is recognized, `false` otherwise.
+   */
+  fun isRecognizedNumberOfControlBytes(numberOfBytes: Int) =
+      inputBytesCountToControlSchemes.containsKey(numberOfBytes)
 }
